58f1cc2f62e508ae1f415c0ab7204772c6df05dc
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-11 06:49:43 +0300

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *main - entry point to the code
+ *puts : print array of string
+ *return : always return 0
+ */
+int main(void)
+{
+char string[] = "Programming is like building a multilingual puzzle\n";
+puts(string);
+return (0);
+}
