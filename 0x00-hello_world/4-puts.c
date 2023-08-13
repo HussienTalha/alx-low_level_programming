82d51beb3259fc20f721d0e2222d144cec39b43f
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-13 14:36:16 +0300

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Main funtion  is entry point to the code
  *
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-char string[] = "Programming is like building a multilingual puzzle\n";
+char string[] = "Programming is like building a multilingual puzzle\n;
 puts(string);
 return (0);
 }
