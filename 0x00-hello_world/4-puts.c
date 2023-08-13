13994af3f8d4b17cc62a5a35d13e0339b3ce2d77
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-13 17:59:29 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Main funtion  is entry point to the code
  *
- * puts: print array of string
- * Return: always return 0
+ * puts: print array of string Return: always return 0
  */
 int main(void)
 {
-char string[] = "Programming is like building a multilingual puzzle\n;
-puts(string);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
