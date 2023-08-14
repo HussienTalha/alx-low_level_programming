c0e9cb64cdd56f3003053b948326bad3e1d9efc4
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-14 16:42:26 +0300

    reverse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar('');
+n++;
+}
+putchar('\n');
+return (0);
+}
