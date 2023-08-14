53708f66953ca94b64c0fef6cfeacbb890d3007e
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-14 16:25:48 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
+}
