81afcaa25fc850501ebed9a0ad6dfdf142d83076
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 00:53:22 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.h"
+/**
+ * main : entry point
+ * 
+ * _putchar : writes the character c to stdout
+ * Return: always return 0
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int i = 0;
+while ( str[i] != '\n')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
