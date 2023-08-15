842f80e216844e40b2299b9e92be7aea04847ecd
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 01:00:24 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main : entry point
- * 
- * _putchar : writes the character c to stdout
+ * main - entry point
+ *
+ * _putchar: writes the character c to stdout
  * Return: always return 0
  */
 int main(void)
 {
 char str[] = "_putchar\n";
 int i = 0;
-while ( str[i] != '\n')
+while (str[i] != '\n')
 {
 _putchar(str[i]);
 i++;
