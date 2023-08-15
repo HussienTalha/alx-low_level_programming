7d9ace67854428250887d585fb4498da2340cc2b
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 01:42:11 +0300

    alphabets 10 time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10() - print alphbets ten times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i = 97;
+int j = 0;
+while (j < 10)
+{
+while (i < 123)
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+j++;
+i = 97;
+}
+}
