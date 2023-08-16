49bde472c3fa91878419d6ec4795edd273e6ccfa
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 04:24:40 +0300

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer - prints time
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+int i = 0;
+int j = 0;
+int k = 0;
+int l = 0;
+while (i <= 2)
+{
+_putchar(i);
+while (j <= 4)
+{
+_putchar(j);
+while (k <= 6)
+{
+_putchar(k);
+while (l <= 9)
+{
+_putchar(l);
+l++;
+}
+k++;
+}
+j++;
+}
+i++;
+}
+}
