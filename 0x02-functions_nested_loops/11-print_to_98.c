8af430b2c3fa25598c8f500f282c5eb8a07d9f44
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 05:56:35 +0300

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,12 @@ if (n <= 98)
 {
 while (n <= 98)
 {
-printf("%d", n);
+printf("%d\n", n);
 if (n == 98)
+{
+putchar('\n');
 break;
+}
 putchar(44);
 putchar(32);
 n++;
@@ -27,7 +30,10 @@ while (n > 98)
 {
 printf("%d", n);
 if (n == 98)
+{
+putchar('\n';)
 break;
+}
 putchar(44);
 putchar(32);
 n--;
