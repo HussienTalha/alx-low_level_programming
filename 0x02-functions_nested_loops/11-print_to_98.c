215531a294dd2f2f6ac95f638a3e7529c1837255
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 05:03:35 +0300

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,35 @@
 #include "main.h"
 /**
+ * print_to_98 - print untill 98
  *
- *
+ * @n: number
+ * _putchar: print
  *
  */
 void print_to_98(int n)
 {
 if (n <= 98)
 {
-_putchar('0' + n);
-if (n == 98)
-break ;
-_putchar(44);
-_putcahr(32);
-n++;
-}
-if (n > 98)
+while (n <= 98)
 {
 _putchar('0' + n);
 if (n == 98)
-break ;
+break;
 _putchar(44);
-_putcahr(32);
+_putchar(32);
+n++;
+}
+}
+if (n > 98)
+{
+while (n > 98)
+{
+_putchar('0' + n);
+if (n == 98)
+break;
+_putchar(44);
+_putchar(32);
 n--;
+}
 }
 }
