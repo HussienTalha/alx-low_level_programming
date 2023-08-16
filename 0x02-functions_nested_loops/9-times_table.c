0af1e317f85f43c0244e1b978396aee121be1073
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 22:14:37 +0300

    times 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - multiply 9 time table
+ * _putchar: print char
+ *
+ */
+void times_table(void)
+{
+int i = 0;
+int j = 0;
+int c = 0;
+int first = 0;
+int last = 0;
+while (i <= 9)
+{
+while (j <= 9)
+{
+c = i * j;
+if (c > 9)
+{
+first = c / 10;
+_putchar(first + '0');
+last = c % 10;
+_putchar(last + '0');
+}
+else
+{
+_putchar(c + '0');
+}
+if (j == 9)
+break;
+_putchar(44);
+_putchar(32);
+j++;
+}
+_putchar('\n');
+i++;
+j = 0;
+}
+}
