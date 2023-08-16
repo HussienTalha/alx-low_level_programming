dfed65112cc6a9464d5c9500ab837db12f569100
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 21:44:16 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @x: first number
+ * @y: second number
+ * Return: return the result
+ */
+int add(int x, int y)
+{
+int z;
+z = x + y;
+return (z);
+return (0);
+}
