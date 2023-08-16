42e1454e2baf7abe5e6ab72c5c219661996c6e02
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-16 03:35:19 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of number
+ * @n: argument 1 get its last digit
+ * Return: return value of last digit and 0
+ */
+
+
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+return (0);
+}
