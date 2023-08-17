19ee00e27096d605a94b6eb4ace41cd382101e20
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-17 19:06:24 +0300

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - print squares
+ * @size: size of square
+ * _putchar: prints
+ *
+ */
+void print_square(int size)
+{
+int k;
+int i;
+k = size;
+while (size > 0)
+{
+for (i = 1; i <= k; i++)
+{
+_putchar(35);
+}
+_putchar('\n');
+size--;
+}
+}
