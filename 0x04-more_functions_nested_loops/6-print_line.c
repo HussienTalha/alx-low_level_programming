b0154652ad28bd54d8a8c7872bd1993dfd5ae008
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-17 18:07:33 +0300

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: number of _
+ * _putchar: prints
+ *
+ *
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar(95);
+n--;
+}
+_putchar('\n');
+}
