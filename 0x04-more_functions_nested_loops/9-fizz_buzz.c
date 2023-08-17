b92d4768a9f92f52529c4f911608f7ad9e782a5c
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-17 20:40:31 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,24 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3) == 0)
-printf("Fizz ");
-else if ((i % 5) == 0)
+if (i % 3 == 0)
 {
-printf("Buzz");
-if (i < 100)
-putchar(32);
+if (i % 5 == 0)
+printf("FizzBuzz ");
+else
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+if (i % 3 == 0)
+printf("FizzBuzz ");
+else if (i == 100)
+printf("Buzz\n");
+else
+printf("Buzz ");
 }
 else
 printf("%d ", i);
 }
-putchar('\n');
 return (0);
 }
