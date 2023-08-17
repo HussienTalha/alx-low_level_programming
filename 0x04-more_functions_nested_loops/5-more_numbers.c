f359109c4ae4cca054608635ba840182434765eb
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-17 16:31:52 +0300

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * more_numbers - print 0-14 10 times
+ * _putchar: prints
+ *
+ *
+ */
+void more_numbers(void)
+{
+int c;
+for (c = 1; c <= 10; c++)
+{
+int i = 0;
+int j = 0;
+while (j <= 1)
+{
+if (j == 0)
+j = 0;
+else
+j = j + 48;
+while (i <= 9)
+{
+_putchar(j);
+_putchar(i + 48);
+if (i == 4 && j > 0)
+break;
+i++;
+}
+if (j > 0  && i == 4)
+break;
+j++;
+}
+_putchar('\n');
+}
+}
