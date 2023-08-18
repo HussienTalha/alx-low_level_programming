1559e8fd3efca1f187d7e38e6f88ca19b03f28ec
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-18 23:44:06 +0300

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,10 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-int 0;
-i = 98;
+int i;
+i = 0;
 positive_or_negative(i);
 return (0);
 }
