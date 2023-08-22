b86401d3889a55e7732a9a49f18d0b29e786b1d9
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-23 00:25:15 +0300

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to another array
+ * @dest: pointer to destination array
+ * @src: pointer to the source
+ * Return: pointer to the dest array
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len = 0;
+while (*src != '\0')
+{
+*dest = *src;
+src++;
+dest++;
+len++;
+}
+*dest = '\0';
+src = src - len;
+dest = src - len;
+return (dest);
+}
