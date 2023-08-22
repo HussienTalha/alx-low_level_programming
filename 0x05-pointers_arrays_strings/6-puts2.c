3b07f5cb0628f402d16107d28b28e6851bdd4c13
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-22 05:46:29 +0300

    each to chrachters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,8 +5,22 @@
  *
  */
 void puts2(char *str)
+
 {
+int len = 0;
+int i = 0;
 while (*str != '\0')
-_putchar(*str);
-s = s + 2;
+{
+len++;
+str++;
 }
+str = str - (len);
+while (i <= len)
+{
+_putchar(*str);
+i += 2;
+str += 2;
+}
+_putchar('\n');
+}
+
