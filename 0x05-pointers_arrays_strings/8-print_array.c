b14d9b3cc017bd2d5a5eb8ce6567e296090ecb63
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-22 06:44:51 +0300

    array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array of integers
+ * @a: pointer to an array
+ * @n: number of elements in the elements
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n-1); i++)
+{
+printf("%d, ", *a);
+a++;
+}
+printf("%d\n", *a);
+}
