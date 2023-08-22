dd81773157e55ae00bfd6d784d1d75e7c130cd8a
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-23 01:05:34 +0300

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,9 @@ char *_strcpy(char *dest, char *src)
 int len = 0;
 while (*src != '\0')
 {
-*dest = *src;
-src++;
-dest++;
+dest[a] = src[a];
 len++;
 }
 *dest = '\0';
-src = src - len;
-dest = src - len;
-return (dest);
 return (dest);
 }
