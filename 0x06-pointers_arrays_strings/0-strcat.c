c5a1549615a56744f613ad4b845b1b460a664308
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-27 00:15:13 +0300

    strcat funtion

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatentae two strings
+ * @dest: pointer to the destination array
+ * @src: pointer to the source array
+ * Return: pointer to destination array
+ *
+ *
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j;
+while (dest[i] != '\0')
+i++;
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+}
+dest[i] = src[j];
+return (dest);
+}
