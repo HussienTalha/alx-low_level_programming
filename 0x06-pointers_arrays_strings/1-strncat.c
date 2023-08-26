5bb96da20c561e0c5afcf390686cbe9b81393a5c
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-27 01:13:44 +0300

    declaration error fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
+int i = 0;
 int j;
 while (dest[i] != '\0')
 i++;
-for (j = 0; j < n; j++)
+for (j = 0; j < n && src[j] != 0; j++)
 {
-dest[i] = src[j];
+dest [i] = src[j];
 i++;
 }
 dest[i] = '\0';
