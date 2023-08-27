33bfaa56f0039fdff4a6d4c193656b4026bf0df0
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-27 04:08:47 +0300

    string comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,22 +3,19 @@
  * _strcmp - compare two string
  * @s1: first string
  * @s2: second string
- * Return: return the comparsion between the length of s1 and s2
+ * Return: return the comparsion between s1 and s2
  *
  *
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (i < j)
-return (-15);
-else if (i == j)
+int i;
+for (i = 0; s1[i] == s2[i]; i++)
+{
+if (s1[i] == '\0')
 return (0);
 else
-return (15);
+continue;
+}
+return (s1[i] - s2[i]);
 }
