5d7e10155d4ecbd958e2fb65d5f1aa16b8767225
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-27 04:46:25 +0300

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - turn lowercase to uppercase
+ * @s: pointer to the string
+ *
+ */
+char *string_toupper(char *s)
+{
+int i;
+int c;
+for (i = 0; s[i] != '\0'; i++)
+{
+c = s[i];
+if (c >= 97 && c <= 122)
+{
+c = c - 32;
+s[i] = c;
+}
+else
+continue;
+}
+return (s);
+}
+
