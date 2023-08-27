b8fb7629d730a35d61f9559c3f99b07c2997e401
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-27 06:44:37 +0300

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string
+ * Return: pointer to string
+ *
+ *
+ */
+char *cap_string(char *s)
+{
+int c;
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+switch (s[i])
+{
+case ' ': case '\t': case '\n': case ',': case ';': case '.':
+case '!': case '?': case '"': case '(': case ')': case '{': case '}':
+{
+c = s[i + 1];
+if (c >= 97 && c <= 122)
+{
+c = c - 32;
+s[i + 1] = c;
+}
+else
+continue;
+break;
+}
+default:
+continue;
+}
+}
+return (s);
+}
