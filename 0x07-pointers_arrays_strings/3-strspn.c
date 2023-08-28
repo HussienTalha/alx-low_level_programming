b1db2a5334d58981cfc73407da16a5ec8efc8e0d
root <root@DESKTOP-NA75PV5.localdomain>
2023-08-29 02:52:03 +0300

    strspn function

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to string
+ * @accept: pointer to perfix substring
+ * Return: position of first char not in accept
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if ( s[i] == accept [j])
+break;
+}
+if (accept[j] == '\0')
+break;
+}
+return (i);
+}
