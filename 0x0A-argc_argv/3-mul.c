fd48456a5912468782b3519a8f3a92751344936f
root <root@DESKTOP-NA75PV5.localdomain>
2023-09-04 02:23:04 +0300

    multiply arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: counter to arguments passed to enviroment
+ * @argv: array of pointer to strings passed to program
+ * Return: 1 if error 0 if run successfully
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int x;
+int result = 1;
+if (argc < 3)
+{
+printf("ERROR\n");
+return (1);
+}
+for (i = 1; i < 3; i++)
+{
+x = strtol(argv[i], NULL, 10); 
+result = result * x;
+}
+printf("%d\n", result);
+return (0);
+}
