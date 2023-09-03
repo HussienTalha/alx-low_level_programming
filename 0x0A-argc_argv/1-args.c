588f20311ed43800542b91eb5bd300a4ca6abf76
root <root@DESKTOP-NA75PV5.localdomain>
2023-09-04 01:41:11 +0300

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
- *
+ * main - number of arguments passed into it
+ * @argc: counter to argrumets passed to program
+ * @argv: array of pointers to strings passed to the program
+ * Return: always return 0 if program run successfully
  *
  *
  */
