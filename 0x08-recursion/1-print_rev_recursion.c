76f3a5b21199bc93123de7e3aeee05d1428fd779
root <root@DESKTOP-NA75PV5.localdomain>
2023-09-10 19:43:27 +0300

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,54 +8,9 @@ void print(char *s, int len);
  */
 void _print_rev_recursion(char *s)
 {
-int len = legth(char *s);
-if (*s == '\0')
-print(s - 1, len);
-else
-s++;
-_print_rev_recursion
-}
-/**
- * length - length of string
- * @s: pointer to the string
- * Return: length of string
- */
-int length(char *s)
+if (*s != '\0')
 {
-int len = 1;
-if (*s == '\0')
-return (0);
-else
-s++;
-len++;
-return (len + length(s));
-}
-/**
- * print - print string
- * @s: pointer to the last digit of the string
- * @len: string length
- */
-print(char *s, int len)
-{
-if (len == 0)
-_putchar('\0');
-else
+_print_rev_recursion(s + 1);
 _putchar(*s);
-print(s - 1, len - 1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
