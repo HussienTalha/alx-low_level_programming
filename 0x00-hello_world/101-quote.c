4b8a86f3eb05ac10c224c1b74491f99f89afa8e5
HussienTalha <hussien.talha99@gmail.com>
2025-05-17 19:55:42 +0300

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: void
+ */
+int main(void)
+{
+	int i = 0;
+	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	while (arr[i] != '\0')
+	{
+		putchar(arr[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
