2aa0ba35de6ca9aa7689fb3bc6b855192e9347c2
HussienTalha <hussien.talha99@gmail.com>
2025-05-22 09:52:34 +0300

    nums combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - entry to the program
+ *
+ *
+ *Return: void
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			continue;
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
