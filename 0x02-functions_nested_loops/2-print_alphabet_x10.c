2e2d3b212217625def45d4bd6c55423f1f7dc983
HussienTalha <hussien.talha99@gmail.com>
2025-05-26 10:31:45 +0300

    betty fixx

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
+ *print_alphabet_x10 - print alphabets 10 times
  *
- *
- *
+ *Return: void
  */
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
 
-	for (i = 1; i <=10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 			_putchar(j);
