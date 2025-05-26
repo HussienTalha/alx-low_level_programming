42632200e818988c18204d9e8b70ede7aeb432c0
HussienTalha <hussien.talha99@gmail.com>
2025-05-26 11:13:28 +0300

    checks the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * print_sign - prints the sign of the num
+ * print_sign - takes int checks the sign
  * @i: number to be checked
  * Return: 1 if positive -1 if negative 0 if zero
  *
@@ -13,7 +13,7 @@ int print_sign(int i)
 		_putchar('+');
 		return (1);
 	}
-	if else(i == 0)
+	else if (i == 0)
 	{
 		_putchar('0');
 		return (0);
