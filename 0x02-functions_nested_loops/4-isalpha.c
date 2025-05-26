e6a86de3648f6cd2858e7c793e500fe9c39cb5e9
HussienTalha <hussien.talha99@gmail.com>
2025-05-26 10:54:29 +0300

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isalpha - checks for charchters
+ *@c: value to be checked
+ *@Return: 1 if charchter 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && 90))
+		return (1);
+	else
+		return (0);
+}
