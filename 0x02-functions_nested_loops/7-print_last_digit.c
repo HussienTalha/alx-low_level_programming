36e4f8e5060556d13e57f31db43407288af46e7f
HussienTalha <hussien.talha99@gmail.com>
2025-05-26 20:35:15 +0300

    fixed the min value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  *print_last_digit - print the last digit
  *@n: the number
@@ -9,10 +10,19 @@
 int print_last_digit(int n)
 {
 	int l;
-
-	if (n < 0)
-		n = n * -1;
-	l = n % 10;
+	if (n < 0 && n > INT_MIN)
+	{
+		n = (n * -1);
+		l = n % 10;
+	}
+	else if (n == INT_MIN)
+	{
+		n = (n + 1) * -1;
+		l = (n % 10) + 1;
+	}
+	else 
+		l = n % 10;
+	
 	_putchar(l + '0');
 	return (l);
 	return (0);
