eb962ac895a7fd920b5f97413f1be1d82465ecd1
HussienTalha <hussien.talha99@gmail.com>
2025-05-30 09:48:31 +0300

    hotfix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  *
- *
- *
+ *_isdigit - check if digit is numiric
+ *@c: digit to be checked
+ *Return: 1 if numiric 0 otherwise
  */
 int _isdigit(int c)
 {
-	if ( 48 >= c <= 57)
+	if (c <= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
