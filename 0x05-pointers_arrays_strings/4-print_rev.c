a4a921e8b00e3571ff3165a7f73969d7d7aaf6a7
HussienTalha <hussien.talha99@gmail.com>
2025-05-31 19:27:21 +0300

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: pointer to the first element of string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	while (len >= 0)
+	{
+		_putchar(*s);
+		len--;
+		s--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,4 +24,5 @@ void reset_to_98(int *ptr);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *s);
+void print_rev(char *s);
 #endif
