9c815342e01fd89b6276200bd8159bbbb910e7ff
HussienTalha <hussien.talha99@gmail.com>
2025-05-31 18:00:04 +0300

    reset by pointer

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int c);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int _isupper(int c);
+int mul(int a, int b);
+int _isdigit(int c);
+void print_numbers(void);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_line(int);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void reset_to_98(int *ptr);
+#endif
