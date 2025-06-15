bc9b9b96343b0198f188d8ba66b4eb408d706cca
HussienTalha <hussien.talha99@gmail.com>
2025-06-15 16:05:03 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && *src != '\0' && *dest != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0' && dest[i] != '\0'; i++)
 		dest[i] = src[i];
-	while (i < n)
+	while (i < n && dest[i] != '\0')
 	{
 		dest[i] = '\0';
 		i++;
