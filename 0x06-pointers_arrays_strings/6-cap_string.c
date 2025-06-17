3601a37fe580fcbc25a441da55a92794d0e4ff09
HussienTalha <hussien.talha99@gmail.com>
2025-06-17 16:03:13 +0300

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all the words
+ * @s: pointer to the string
+ * Return: pointer to the string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		switch (s[i])
+		{
+			case ' ': case '\t':
+			case '\n': case ',':
+			case ';': case '.':
+			case '!': case '?':
+			case '"': case '(':
+			case ')': case '{':
+			case '}':
+				{
+					if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					{
+						s[i + 1] = s[i + 1] - 32;
+					}
+				}
+		}
+	}
+	return (s);
+}
