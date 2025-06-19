9fbba9574914bc5039a15e36d4f78cbf9e5a7469
HussienTalha <hussien.talha99@gmail.com>
2025-06-19 06:40:55 +0300

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strspn - calculates the length of initial segment of string 
+ * _strspn - calculates the length of initial segment of string
  * that consists only of accept
  * @s: pointer to string
  * @accept: pointer to the subset of charchter the substring consist of
@@ -8,20 +8,20 @@
  */
 int _strspn(char *s, char *accept)
 {
-	int i ,j, n;
+int i, j, n;
 
-	n = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{	
-		for (j = 0; accept[j] != '\0'; j++)
-			{
-				if (accept[j] == s[i])
-					break;
-			}
-		if (accept[j] != '\0')
-			n++;
-		else
-			return (n);
-	}
-	return (n);
+n = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+break;
+}
+if (accept[j] != '\0')
+n++;
+else
+return (n);
+}
+return (n);
 }
