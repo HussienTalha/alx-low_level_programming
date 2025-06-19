5fdf75ae4c2dcd3af188cfdcba510c2ba94dcc21
HussienTalha <hussien.talha99@gmail.com>
2025-06-19 06:38:12 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - calculates the length of initial segment of string 
+ * that consists only of accept
+ * @s: pointer to string
+ * @accept: pointer to the subset of charchter the substring consist of
+ * Return: length of the perfix substring
+ */
+int _strspn(char *s, char *accept)
+{
+	int i ,j, n;
+
+	n = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{	
+		for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (accept[j] == s[i])
+					break;
+			}
+		if (accept[j] != '\0')
+			n++;
+		else
+			return (n);
+	}
+	return (n);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -40,4 +40,6 @@ char *cap_string(char *);
 char *leet(char *s);
 char *_memset(char *s, char b, int n);
 char *_memcpy(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
 #endif
