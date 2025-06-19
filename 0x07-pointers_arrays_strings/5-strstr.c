44c1fbee86ef2d37413803e17c55cae2b0591d0b
HussienTalha <hussien.talha99@gmail.com>
2025-06-20 02:44:23 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - finds the first occurance of substring needle in string haystack
+ * @haystack: string to be searched in
+ * @needle: substring to be sarched for in
+ * Return: pointer to the begining of the located substring in haystack or NULL
+ * if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		if (*haystack != needle[0])
+			continue;
+		else
+		i = 0;
+		{
+			for (j = 0; haystack[j] == needle[i] && needle[i] != '\0'; j++)
+			{
+				i++;
+			}
+			if (needle[i] == '\0')
+				return (haystack);
+		}
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -43,4 +43,5 @@ char *_memcpy(char *dest, char *src, int n);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
